c141d9bf7357592f4df630f581b4868e06597604
shonjmoj <midoounejjart@gmail.com>
2023-01-24 13:04:55 +0100

    fix : wrap all commands into a script file

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
-mysql -e "CREATE DATABASE wordpress_db;" 
-mysql -e "CREATE USER 'shonjmoj'@'localhost' IDENTIFIED BY '123';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'shonjmoj'@'localhost' WITH GRANT OPTION;"
-mysql -e "CREATE USER 'shonjmoj'@'%' IDENTIFIED BY '123';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'shonjmoj'@'%' WITH GRANT OPTION;"
-mysql -e "FLUSH PRIVILEGES;"
+if [ ! -d "/var/lib/mysql/wordpress_db" ]; then
+    service mysql start
+
+    mysql -e "CREATE DATABASE wordpress_db;"
+
+    mysql -e "CREATE USER 'shonjmoj'@'localhost' IDENTIFIED BY '123';"
+    mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'shonjmoj'@'localhost' WITH GRANT OPTION;"
+    mysql -e "CREATE USER 'shonjmoj'@'%' IDENTIFIED BY '123';"
+    mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'shonjmoj'@'%' WITH GRANT OPTION;"
+    mysql -e "FLUSH PRIVILEGES;"
+
+    mysql wordpress_db < db.sql
+
+    service mysql stop
+fi
+
+mysqld_safe
